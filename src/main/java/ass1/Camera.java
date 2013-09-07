09f71bc5df255382522db6ae8ef3dd43dfefa8c7
Lem <lemmie6@gmail.com>
2013-09-07 17:10:45 +1000

    camera done

--- src/main/java/ass1/Camera.java
+++ src/main/java/ass1/Camera.java
@@ -2,6 +2,7 @@ package ass1;
 
 import javax.media.opengl.GL;
 import javax.media.opengl.GL2;
+import javax.media.opengl.glu.GLU;
 
 /**
  * The camera is a ass1.GameObject that can be moved, rotated and scaled like any other.
@@ -48,14 +49,26 @@ public class Camera extends GameObject {
         // TODO 2. set the view matrix to account for the camera's position
         gl.glMatrixMode(GL2.GL_MODELVIEW);
         gl.glLoadIdentity();
-        gl.glScaled(1.0 / getScale(), 1.0 / getScale(), 1);
-        gl.glRotated(-getRotation(), 0, 0, 1);
-        gl.glTranslated(-getPosition()[0], -getPosition()[1], 0);
+        gl.glScaled(1.0 / getGlobalScale(), 1.0 / getGlobalScale(), 1);
+        gl.glRotated(-getGlobalRotation(), 0, 0, 1);
+        gl.glTranslated(-getGlobalPosition()[0], -getGlobalPosition()[1], 0);
     }
 
     public void reshape(GL2 gl, int x, int y, int width, int height) {
         // TODO  1. match the projection aspect ratio to the viewport
         // to avoid stretching
-        
+
+        // source: Malcolm Ryan's Triangle Example :D
+        // calculate the aspect ratio of window
+        double aspect = 1d * width / height;
+
+        // in OpenGL terms, we are changing the 'projection'
+        // this will be explained later
+        gl.glMatrixMode(GL2.GL_PROJECTION);
+        gl.glLoadIdentity();
+
+        // Use the GLU library to compute the new projection
+        GLU glu = new GLU();
+        glu.gluOrtho2D(-aspect, aspect, -1.0, 1.0);  // left, right, top, bottom
     }
 }
